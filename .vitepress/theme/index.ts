a734ac91ea0fb3506f811fb920beaa8f13e80872
zerolee <464806884@qq.com>
2023-11-16 18:48:27 +0800

    feat: 添加图片放大功能

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,6 +3,7 @@ import { h } from 'vue';
 import type { Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
 import { useData } from 'vitepress';
+import ImageModel from '../../src/components/imgmodel.vue';
 import './style.css';
 import 'uno.css';
 
@@ -10,9 +11,12 @@ export default {
   extends: DefaultTheme,
   Layout: () => {
     const { lang } = useData();
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    });
+    return [
+      h(DefaultTheme.Layout, null, {
+        // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      }),
+      h(ImageModel),
+    ];
   },
   enhanceApp({ app, router, siteData }) {
     // ...
