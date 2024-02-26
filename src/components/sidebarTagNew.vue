38325f3197066fcc25f6299de18c28c2597031b2
zerolee <464806884@qq.com>
2024-02-26 11:55:37 +0800

    feat: remove newtag after enter

--- src/components/sidebarTagNew.vue
+++ src/components/sidebarTagNew.vue
@@ -1,21 +1,35 @@
+<template></template>
 <script setup lang="ts">
-import { diff } from '@/utils/diff';
+import { diff, currendBranch } from '@/utils/diff';
 const fileArr = diff
   .split('\n')
   .map((_) => _.replace('src/', '').replace('.md', '.html'));
+const cacheKey = `${currendBranch}:cachenew`;
+
+const cached = ref<string[]>([]);
+
+try {
+  const d = JSON.parse(localStorage.getItem(cacheKey) || '');
+  cached.value = d;
+} catch (e) {}
 
 onMounted(() => {
   const links = document.querySelectorAll('.VPSidebarItem .VPLink');
   links.forEach((ele) => {
     const el = ele as HTMLAnchorElement;
     const includes = fileArr.find((_) => el.href.includes(_));
-    if (!includes) {
+    if (!includes || cached.value.includes(el.href)) {
       return;
     }
     const span = document.createElement('span');
     span.classList.add('sidebarTagNew');
-
     el?.append(span);
+
+    el.addEventListener('click', () => {
+      span.remove();
+      cached.value.push(el.href);
+      localStorage.setItem(cacheKey, JSON.stringify(cached.value));
+    });
   });
 });
 </script>
@@ -31,5 +45,6 @@ onMounted(() => {
   height: 11.6px;
   background-image: url(../public/new.png);
   background-size: 100% 100%;
+  background-color: red;
 }
 </style>
